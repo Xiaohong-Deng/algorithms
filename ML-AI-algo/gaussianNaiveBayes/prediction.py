52233bb9148e68940fb9a94dd39d83b312cd1013
Xiaohong-Deng <worldsky86rough@gmail.com>
2018-05-14 20:02:13 +0800

    clean up GNB predict code

--- ML-AI-algo/gaussianNaiveBayes/prediction.py
+++ ML-AI-algo/gaussianNaiveBayes/prediction.py
@@ -1,37 +1,30 @@
 #!/usr/bin/env python
-
 import classifier
-from importlib import reload
 import json
-# gnb = GNB()
-# with open('train.json', 'rb') as f:
-# 	j = json.loads(f.read().decode('utf8'))
-# print(j.keys())
-# X = j['states']
-# Y = j['labels']
-# print(Y[:5])
-reload(classifier)
+
+
 def main():
-	gnb = classifier.GNB()
-	with open('train.json', 'rb') as f:
-		j = json.loads(f.read().decode('utf8'))
-	print(j.keys())
-	X = j['states']
-	Y = j['labels']
-	gnb.train(X, Y)
+  gnb = classifier.GNB()
+  with open('train.json', 'rb') as f:
+    j = json.loads(f.read().decode('utf8'))
+  print(j.keys())
+  X = j['states']
+  Y = j['labels']
+  gnb.train(X, Y)
 
-	with open('test.json', 'rb') as f:
-		j = json.loads(f.read().decode('utf8'))
+  with open('test.json', 'rb') as f:
+    j = json.loads(f.read().decode('utf8'))
 
-	X = j['states']
-	Y = j['labels']
-	score = 0
-	for coords, label in zip(X,Y):
-		predicted = gnb.predict(coords)
-		if predicted == label:
-			score += 1
-	fraction_correct = float(score) / len(X)
-	print("You got {} percent correct".format(100 * fraction_correct))
+  X = j['states']
+  Y = j['labels']
+  score = 0
+  for coords, label in zip(X, Y):
+    predicted = gnb.predict(coords)
+    if predicted == label:
+      score += 1
+  fraction_correct = float(score) / len(X)
+  print("You got {} percent correct".format(100 * fraction_correct))
+
 
 if __name__ == "__main__":
-	main()
+  main()
